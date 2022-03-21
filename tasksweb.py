fd7e0f62e3626cd9c74c8e4495d6234768a5b518
Tushar Maharana <tusharmaharana44676@gmail.com>
2022-03-21 07:14:11 +0530

    fixed it finally ig

--- tasksweb.py
+++ tasksweb.py
@@ -3,5 +3,12 @@
 this script will be used to automatically create the files in my computer and then push it to git :)
 '''
 import os#this is very important for us to issue commands
-os.system(gittasks)#using another script to get the material into tasks.txt
-cid = open("unicode.txt",'rt')
+os.system('gittasks')#using another script to get the material into tasks.txt
+tasks = open("tasks.txt",'rt')
+tasks = tasks.read()
+html = open("html.txt",'rt')
+html = html.read()
+html2 = open("html2.txt",'rt')
+html2 = html2.read()
+tasksjs = str(html2) + str(tasks) + str(html)
+print(tasksjs)
