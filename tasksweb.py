4904ee828830e42c15068fd81b85040810f67ba8
Tushar Maharana <tusharmaharana44676@gmail.com>
2022-03-21 07:03:46 +0530

    fixed it finally ig

---
+++ tasksweb.py
@@ -0,0 +1,7 @@
+#! /bin/python3
+'''
+this script will be used to automatically create the files in my computer and then push it to git :)
+'''
+import os#this is very important for us to issue commands
+os.system(gittasks)#using another script to get the material into tasks.txt
+
