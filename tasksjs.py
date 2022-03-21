923076eb0e2a5eb7eefdc23ba8c242ab5515f9c6
Tushar Maharana <tusharmaharana44676@gmail.com>
2022-03-21 07:35:03 +0530

    routine daily tasks

--- tasksjs.py
+++ tasksjs.py
@@ -4,15 +4,15 @@ this script will be used to automatically create the task.js in my computer and 
 '''
 import os#this is very important for us to issue commands
 os.system('gittasks')#using another script to get the material into tasks.txt
-tasks = open("tasks",'rt')
+tasks = open("/home/tusharhero/Documents/tasks/tasks",'rt')
 tasks = tasks.read()
-html = open("html.txt",'rt')
+html = open("/home/tusharhero/Documents/tasks/html.txt",'rt')
 html = html.read()
-html2 = open("html2.txt",'rt')
+html2 = open("/home/tusharhero/Documents/tasks/html2.txt",'rt')
 html2 = html2.read()
 tasksjs = str(html2) + str(tasks) + str(html)
 print(tasksjs)
-f = open("tasks.js", "w")
+f = open("/home/tusharhero/Documents/tasks/tasks.js", "w")
 f.write(tasksjs)
 f.close()
-os.system("cd ~/Documents/tasks & git commit -a -m 'routine daily tasks' & git push")
+os.system("cd ~/Documents/tasks & git -C ~/Documents/tasks commit -a -m 'routine daily tasks' & git -C ~/Documents/tasks push")
