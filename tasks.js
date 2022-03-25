0baa8762bc91b690c2f168df167df142578dc4e9
Tushar Maharana <tusharmaharana44676@gmail.com>
2022-03-25 08:16:31 +0530

    routine daily tasks

--- tasks.js
+++ tasks.js
@@ -2,12 +2,15 @@ let text = `
 03/25/22 Friday
 1) English 
 --> Modals
++ Determiner
 2) Social --> 
 Nationalism in India
++ Outcomes of Democracy
 3) Maths 
 --> Circles
 4) Hindi -->
 सूचनाओ
++Read big story hehe
 `;
 console.log(text);
 let header = document.querySelector("tasks");
